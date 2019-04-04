085bede3a9e3375187a5b29ac3a1a79aa0d201e3
Leonardas Persaud <wingdragon_9@hotmail.com>
2019-04-04 14:32:58 +0200

    added random 6 number generator

--- oracle.sol
+++ oracle.sol
@@ -1,5 +1,25 @@
 pragma solidity <=0.5.10;
 
 contract Oracle {
+    
+  uint256[6] winning_number;
+
+  function setNum(uint256[6] memory num) private {
+      winning_number = num;
+  }
+
+  function getNum() public view returns (uint256[6] memory) {
+      return winning_number;
+  }
+
+  function random() public {
+      uint256 num = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%(10**12);
+      uint256[6] memory arr;
+      for(uint256 i=0; i < 6; i++){
+          arr[i] = num%100;
+          num /= 100;
+      }
+      setNum(arr);
+  }
 
 }
