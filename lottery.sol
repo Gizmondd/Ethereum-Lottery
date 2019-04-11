ffb7e10a03d4e80487fbd9b1ecabd17e19aca69b
kvernknurr <lukas-yu@hotmail.com>
2019-04-11 14:38:52 +0200

    Update lottery.sol

--- lottery.sol
+++ lottery.sol
@@ -1,25 +1,53 @@
 pragma solidity <=0.5.10;
 
+contract Oracle {
+    uint constant draw_count = 6;
+    function generate_number() public returns (uint[draw_count] memory);
+}
+
 contract Lottery {
     address owner;
-    mapping (uint8 => address[]) tickets;
-    mapping (address => uint) winnings;
-    uint pot;
+    mapping (uint => address payable[]) tickets;
+    uint prize_pot;
+    bool active;
+    uint draw_time;
+    uint constant draw_count = 6;
+    Oracle oracle;
+    
 
-    constructor() public {
+    constructor(address _oracle) public {
         owner = msg.sender;
+        active = false;
+        oracle = Oracle(_oracle);
+    }
+    
+    function start_lottery(uint time) public {
+        require(msg.sender == owner);
+        require(!active);
+        
+        draw_time = now + time;
+        // tickets = empty??
+        active = true;
     }
 
-    function buy_ticket(uint8 number) public payable {
+    function buy_ticket(uint[draw_count] memory ticket_numbers) public payable {
+        require(active);
         require(msg.value == 1);
-        require(number < 100);
 
-        pot += msg.value;
-        tickets[number].push(msg.sender);
+        prize_pot += msg.value;
+        uint ticket_number_sorted = sort_and_combine(ticket_numbers); 
+        
+        tickets[ticket_number_sorted].push(msg.sender);
     }
 
-    function end_lottery(uint8 winning_numbers) public {
-        address[] memory winners = tickets[winning_numbers];
+    function end_lottery() public {
+        require(active);
+        require(now < draw_time);
+        active = false;
+        
+        uint winning_number_sorted = sort_and_combine(oracle.generate_number());
+        
+        address payable[] memory winners = tickets[winning_number_sorted];
         uint num_winners = winners.length;
 
         if (num_winners == 0) {
@@ -27,33 +55,20 @@ contract Lottery {
         } else {
             // Split the pot as fair as possible
             // Keep the remainder of the division in the pot
-            uint prize = pot / num_winners;
-            pot = pot % num_winners;
+            uint prize = prize_pot / num_winners;
+            prize_pot = prize_pot % num_winners;
 
-            for (uint i = 0; i>num_winners; i++) {
-                winnings[winners[i]] += prize;
+            for (uint i = 0; i<num_winners; i++) {
+                winners[i].transfer(prize);
             }
         }
     }
-
-    function reclaim_prize() public returns (bool) {
-        uint amount = winnings[msg.sender];
-        if (amount > 0) {
-            // It is important to set this to zero because the recipient
-            // can call this function again as part of the receiving call
-            // before 'send' returns.
-            winnings[msg.sender] = 0;
-
-            if (!msg.sender.send(amount)) {
-                // No need to call throw here, just reset the amount owing
-                winnings[msg.sender] = amount;
-                return false;
-            }
+    
+    function sort_and_combine(uint[draw_count] memory ticket_numbers) internal pure returns (uint) {
+        uint result = 0;
+        for (uint i = 0; i<draw_count; i++) {
+            result = ticket_numbers[i] * 100 ** i;
         }
-        return true;
-    }
-
-    function get_pot() public view returns (uint) {
-        return pot;
+        return result;
     }
 }
