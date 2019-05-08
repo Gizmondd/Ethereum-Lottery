1a3c476b7bd9bc1acaf11cf1af40822f2083baf2
ronic09 <ronic09@gmx.ch>
2019-05-08 20:54:48 +0200

    Update lottery.sol

--- lottery.sol
+++ lottery.sol
@@ -32,6 +32,15 @@ contract Lottery {
         return round_number;
     }
     
+    function get_Pot() view public returns (uint){
+        return prize_pot;
+    }
+    
+    function get_winningNumbers() view public returns (uint){
+        return winning_number_sorted;
+    }
+
+    
     
     function sort_and_merge(uint[draw_count] memory ticket_numbers) pure public returns (uint) {
         uint result = 0;
@@ -92,33 +101,45 @@ contract Lottery {
             tickets_closed = true;
             blockNr = block.number;
         } else {
-            require(blockNr != block.number, "Still on the same block");
-            round_number += 1;
-            active = false;
-            // Executed when the transaction is on a new block
-            
-            // Generate winning numbers based on new block number
-            winning_number_sorted = generate_number();
-            
-            address payable[] memory winners = tickets[round_number][winning_number_sorted];
-            uint num_winners = winners.length;
-            
-            uint owner_share = prize_pot / 100;
-            prize_pot -= owner_share;
-            owner.transfer(owner_share);
-            
-            if (num_winners != 0) {
-                // Split the pot as fair as possible
-                // Keep the remainder of the division in the pot
-                uint prize = prize_pot / num_winners;
-                prize_pot = prize_pot % num_winners;
-    
-                for (uint i = 0; i<num_winners; i++) {
-                    winners[i].transfer(prize);
-                }
-            }
-            
+            require(tickets_closed, "The lottery has already ended");
         }
-        
     }
+    
+    function determine_winner() public {
+        require(tickets_closed, "The lottery is still active");
+        require(blockNr != block.number, "Still on the same block");
+
+        active = false;
+        // Executed when the transaction is on a new block
+            
+        // Generate winning numbers based on new block number
+        //winning_number_sorted = generate_number();
+        uint[draw_count] memory numbers;
+        numbers[0] = 11;
+        numbers[1] = 22;
+        numbers[2] = 33;
+        numbers[3] = 44;
+        numbers[4] = 55;
+        numbers[5] = 66;
+        winning_number_sorted = sort_and_merge(numbers);
+            
+        address payable[] memory winners = tickets[round_number][winning_number_sorted];
+        uint num_winners = winners.length;
+        round_number += 1;
+        uint owner_share = prize_pot / 100;
+        prize_pot -= owner_share;
+        owner.transfer(owner_share);
+            
+        if (num_winners != 0) {
+            // Split the pot as fair as possible
+            // Keep the remainder of the division in the pot
+            uint prize = prize_pot / num_winners;
+            prize_pot = prize_pot % num_winners;
+    
+            for (uint i = 0; i<num_winners; i++) {
+                winners[i].transfer(prize);
+            }
+        }    
+    }
+
 }
